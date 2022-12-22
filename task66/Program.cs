5d1f02bb9659bc74efc8c4d717102552bd2ef235
fitmeat <eenot825@gmail.com>
2022-12-22 15:52:47 +0300

    Выполнил задание 66

---
+++ task66/Program.cs
@@ -0,0 +1,11 @@
+﻿int rec(int M, int N)
+{
+    if(N<M) return 0;
+    return rec(M, N-1) + N;
+}
+Console.Clear();
+Console.WriteLine("Enter M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(M,N));
