a7253dba13d40c66177a12dd77f92c9d3e27d0f3
fitmeat <eenot825@gmail.com>
2022-12-22 16:25:58 +0300

    Выполнил задание 68

---
+++ task68/Program.cs
@@ -0,0 +1,14 @@
+﻿int A(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return A(m - 1, 1);
+    if (m > 0 && n > 0) return A(m - 1, A(m, n - 1));
+    return A(m, n);
+}
+
+Console.Clear();
+Console.WriteLine("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(A(m, n));
